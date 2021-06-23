3277864bbb809627c4beb924ac1223aca1fc5f5b
jim <303600370@qq.com>
2021-06-23 22:40:13 +0800

    getRepo

--- engine/buildtask_test.go
+++ engine/buildtask_test.go
@@ -13,8 +13,8 @@ func TestGitClone(t *testing.T) {
 		build: &runtime.Build{
 			Id: "1231",
 			Repo: &runtime.Repository{
-				Name:     "303600370@qq.com",
-				Token:    "jimbir6666memory",
+				Name:     "",
+				Token:    "",
 				Sha:      "c202ee042db1fc8b8c16c6c968195cec6185d7db",
 				CloneURL: "https://gitee.com/SuperHeroJim/gokins-test",
 			},
