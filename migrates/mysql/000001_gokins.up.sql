9e2ee3f09e3a53b66c939d4e5755cf5224a53d33
Linsk.Oldbook <mgr9525@qq.com>
2021-06-25 16:37:19 +0800

    orgs

--- migrates/mysql/000001_gokins.up.sql
+++ migrates/mysql/000001_gokins.up.sql
@@ -1,6 +1,3 @@
--- ----------------------------
--- Table structure for t_build
--- ----------------------------
 CREATE TABLE `t_build` (
   `id` varchar(64) NOT NULL,
   `pipeline_id` varchar(64) NULL DEFAULT NULL,
@@ -31,21 +28,26 @@ CREATE TABLE `t_cmd_line` (
   `finished` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 );
-CREATE TABLE `t_hook` (
+CREATE TABLE `t_stage` (
   `id` varchar(64) NOT NULL,
-  `type` varchar(255) NULL DEFAULT NULL,
-  `snapshot` longtext NULL,
-  `status` varchar(255) NULL DEFAULT NULL,
-  `msg` varchar(255) NULL DEFAULT NULL,
-  `hook_type` varchar(255) NULL DEFAULT NULL,
+  `pipeline_version_id` varchar(64) NULL DEFAULT NULL COMMENT '流水线id',
+  `build_id` varchar(64) NULL DEFAULT NULL,
+  `status` varchar(100) NULL DEFAULT NULL COMMENT '构建状态',
+  `exit_code` bigint(20) NULL DEFAULT NULL COMMENT '退出码',
+  `error` varchar(500) NULL DEFAULT NULL COMMENT '错误信息',
+  `name` varchar(255) NULL DEFAULT NULL COMMENT '名字',
+  `display_name` varchar(255) NULL DEFAULT NULL,
+  `started` datetime(0) NULL DEFAULT NULL COMMENT '开始时间',
+  `finished` datetime(0) NULL DEFAULT NULL COMMENT '结束时间',
+  `created` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `updated` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `version` varchar(255) NULL DEFAULT NULL COMMENT '版本',
+  `on_success` varchar(5) NULL DEFAULT NULL,
+  `on_failure` varchar(5) NULL DEFAULT NULL,
+  `sort` bigint(10) NULL DEFAULT NULL,
+  `stage` varchar(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 );
--- ----------------------------
--- Records of t_hook
--- ----------------------------
--- ----------------------------
--- Table structure for t_step
--- ----------------------------
 CREATE TABLE `t_step` (
   `id` varchar(64) NOT NULL,
   `build_id` varchar(64) NULL DEFAULT NULL,
@@ -71,15 +73,9 @@ CREATE TABLE `t_step` (
   `sort` bigint(10) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 );
--- ----------------------------
--- Records of t_step
--- ----------------------------
--- ----------------------------
--- Table structure for t_message
--- ----------------------------
 CREATE TABLE `t_message` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `xid` varchar(64) NOT NULL,
+  `id` varchar(64) NOT NULL,
+  `aid` BIGINT NOT NULL AUTO_INCREMENT,
   `uid` varchar(64) NULL DEFAULT NULL COMMENT '发送者（可空）',
   `title` varchar(255) NULL DEFAULT NULL,
   `content` longtext NULL,
@@ -87,282 +83,92 @@ CREATE TABLE `t_message` (
   `created` datetime(0) NULL DEFAULT NULL,
   `infos` text NULL,
   `url` varchar(500) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`, `xid`) USING BTREE
+  PRIMARY KEY (`id`, `aid`) USING BTREE
 );
--- ----------------------------
--- Records of t_message
--- ----------------------------
--- ----------------------------
--- Table structure for t_param
--- ----------------------------
+CREATE TABLE `t_org` (
+  `id` varchar(64) NOT NULL,
+  `aid` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NULL DEFAULT NULL,
+  `title` varchar(200) NULL DEFAULT NULL,
+  `desc` TEXT NULL DEFAULT NULL,
+  `public` INT(1) NULL DEFAULT 0 COMMENT '公开',
+  `created` datetime(0) NULL DEFAULT NULL,
+  `deleted` int(1) NULL DEFAULT 0,
+  `deleted_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `aid`) USING BTREE
+);
+CREATE TABLE `t_org_pipe` (
+  `aid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `org_id` varchar(64) NULL DEFAULT NULL,
+  `pipe_id` varchar(64) NULL DEFAULT NULL COMMENT '收件人',
+  `created` datetime(0) NULL DEFAULT NULL,
+  `public` INT(1) NULL DEFAULT 0 COMMENT '公开',
+  PRIMARY KEY (`aid`) USING BTREE,
+  INDEX `org_id`(`org_id`) USING BTREE
+);
+CREATE TABLE `t_pipeline` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(255) NULL DEFAULT NULL,
+  `repo_id` varchar(64) NULL DEFAULT NULL,
+  `display_name` varchar(255) NULL DEFAULT NULL,
+  `pipeline_type` varchar(255) NULL DEFAULT NULL,
+  `json_content` longtext NULL,
+  PRIMARY KEY (`id`) USING BTREE
+);
+CREATE TABLE `t_pipeline_version` (
+  `id` varchar(64) NOT NULL,
+  `number` bigint(20) NULL DEFAULT NULL COMMENT '构建次数',
+  `trigger` varchar(100) NULL DEFAULT NULL COMMENT '触发方式',
+  `events` varchar(100) NULL DEFAULT NULL COMMENT '事件push、pr、note',
+  `ref` varchar(255) NULL DEFAULT NULL,
+  `branch` varchar(255) NULL DEFAULT NULL,
+  `repo_id` varchar(64) NULL DEFAULT NULL,
+  `repo_name` varchar(255) NULL DEFAULT NULL,
+  `commit_sha` varchar(255) NULL DEFAULT NULL,
+  `commit_message` text NULL COMMENT '提交信息',
+  `pipeline_name` varchar(255) NULL DEFAULT NULL,
+  `pipeline_display_name` varchar(255) NULL DEFAULT NULL,
+  `pipeline_id` varchar(64) NULL DEFAULT NULL,
+  `version` varchar(255) NULL DEFAULT NULL,
+  `yml_content` longtext NULL,
+  `created` datetime(0) NULL DEFAULT NULL,
+  `create_user` varchar(255) NULL DEFAULT NULL,
+  `create_user_id` varchar(64) NULL DEFAULT NULL,
+  `deleted` tinyint(1) NULL DEFAULT 0,
+  `target_repo_name` varchar(255) NULL DEFAULT NULL,
+  `target_repo_sha` varchar(255) NULL DEFAULT NULL,
+  `target_repo_ref` varchar(255) NULL DEFAULT NULL,
+  `target_repo_clone_url` varchar(255) NULL DEFAULT NULL,
+  `status` varchar(100) NULL DEFAULT NULL COMMENT '构建状态',
+  `error` varchar(500) NULL DEFAULT NULL COMMENT '错误信息',
+  `note` varchar(255) NULL DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `pr_number` bigint(20) DEFAULT NULL,
+  `repo_clone_url` varchar(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+);
 CREATE TABLE `t_param` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `aid` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NULL DEFAULT NULL,
   `title` varchar(255) NULL DEFAULT NULL,
   `data` text NULL,
   `times` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`aid`) USING BTREE
 );
--- ----------------------------
--- Records of t_param
--- ----------------------------
--- ----------------------------
--- Table structure for t_permssion
--- ----------------------------
-CREATE TABLE `t_permssion` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `xid` varchar(64) NOT NULL,
-  `parent` varchar(64) NULL DEFAULT NULL,
-  `title` varchar(100) NULL DEFAULT NULL,
-  `value` varchar(100) NULL DEFAULT NULL,
-  `times` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
-  `sort` int(11) NULL DEFAULT 10,
-  PRIMARY KEY (`id`, `xid`) USING BTREE,
-  INDEX `IDX_sys_permssion_parent`(`parent`) USING BTREE
-);
--- ----------------------------
--- Records of t_permssion
--- ----------------------------
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    1,
-    'common',
-    NULL,
-    '通用',
-    'common',
-    '2019-10-23 11:29:32',
-    0
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    2,
-    'login',
-    'common',
-    '登录',
-    'login',
-    '2019-10-23 11:29:40',
-    11
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    3,
-    'uppass',
-    'common',
-    '修改密码',
-    'comm:uppass',
-    '2020-03-01 21:47:42',
-    12
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    4,
-    'admin',
-    NULL,
-    '后台界面',
-    'admin',
-    '2019-10-27 00:11:30',
-    2
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    5,
-    'roles',
-    NULL,
-    '角色管理',
-    'role:list',
-    '2019-11-03 13:48:58',
-    3
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    6,
-    'role1',
-    'roles',
-    '角色编辑',
-    'role:edit',
-    '2019-11-03 13:49:21',
-    11
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    7,
-    'role2',
-    'roles',
-    '角色删除',
-    'role:del',
-    '2019-11-03 13:49:22',
-    12
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    8,
-    'grant',
-    'roles',
-    '用户授权',
-    'user:grant',
-    '2019-11-04 09:52:47',
-    13
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    9,
-    'users',
-    NULL,
-    '用户管理',
-    'user:list',
-    '2019-11-04 09:51:14',
-    4
-  );
-INSERT INTO
-  `t_permssion`
-VALUES
-  (
-    10,
-    'userxg',
-    'users',
-    '用户编辑',
-    'user:edit',
-    '2019-11-04 20:27:02',
-    10
-  );
--- ----------------------------
-  -- Table structure for t_pipeline
-  -- ----------------------------
-  CREATE TABLE `t_pipeline` (
-    `id` varchar(64) NOT NULL,
-    `name` varchar(255) NULL DEFAULT NULL,
-    `repo_id` varchar(64) NULL DEFAULT NULL,
-    `display_name` varchar(255) NULL DEFAULT NULL,
-    `pipeline_type` varchar(255) NULL DEFAULT NULL,
-    `json_content` longtext NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
-CREATE TABLE `t_pipeline_version` (
-    `id` varchar(64) NOT NULL,
-    `number` bigint(20) NULL DEFAULT NULL COMMENT '构建次数',
-    `trigger` varchar(100) NULL DEFAULT NULL COMMENT '触发方式',
-    `events` varchar(100) NULL DEFAULT NULL COMMENT '事件push、pr、note',
-    `ref` varchar(255) NULL DEFAULT NULL,
-    `branch` varchar(255) NULL DEFAULT NULL,
-    `repo_id` varchar(64) NULL DEFAULT NULL,
-    `repo_name` varchar(255) NULL DEFAULT NULL,
-    `commit_sha` varchar(255) NULL DEFAULT NULL,
-    `commit_message` text NULL COMMENT '提交信息',
-    `pipeline_name` varchar(255) NULL DEFAULT NULL,
-    `pipeline_display_name` varchar(255) NULL DEFAULT NULL,
-    `pipeline_id` varchar(64) NULL DEFAULT NULL,
-    `version` varchar(255) NULL DEFAULT NULL,
-    `yml_content` longtext NULL,
-    `created` datetime(0) NULL DEFAULT NULL,
-    `create_user` varchar(255) NULL DEFAULT NULL,
-    `create_user_id` varchar(64) NULL DEFAULT NULL,
-    `deleted` tinyint(1) NULL DEFAULT 0,
-    `target_repo_name` varchar(255) NULL DEFAULT NULL,
-    `target_repo_sha` varchar(255) NULL DEFAULT NULL,
-    `target_repo_ref` varchar(255) NULL DEFAULT NULL,
-    `target_repo_clone_url` varchar(255) NULL DEFAULT NULL,
-    `status` varchar(100) NULL DEFAULT NULL COMMENT '构建状态',
-    `error` varchar(500) NULL DEFAULT NULL COMMENT '错误信息',
-    `note` varchar(255) NULL DEFAULT NULL,
-    `title` varchar(255) DEFAULT NULL,
-    `pr_number` bigint(20) DEFAULT NULL,
-    `repo_clone_url` varchar(255) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
--- ----------------------------
-  -- Table structure for t_role
-  -- ----------------------------
-  CREATE TABLE `t_role` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `xid` varchar(64) NOT NULL,
-    `title` varchar(100) NULL DEFAULT NULL,
-    `perms` text NULL,
-    `times` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
-    PRIMARY KEY (`id`, `xid`) USING BTREE
-  );
--- ----------------------------
-  -- Records of t_role
-  -- ----------------------------
-INSERT INTO
-  `t_role`
-VALUES
-  (
-    1,
-    'common',
-    '通用权限',
-    'common,login,uppass',
-    '2019-10-23 11:31:34'
-  );
-INSERT INTO
-  `t_role`
-VALUES
-  (
-    2,
-    'admin',
-    '权限管理员',
-    'admin,roles,role1,role2,users,grant',
-    '2019-11-03 13:49:35'
-  );
--- ----------------------------
-  -- Table structure for t_stage
-  -- ----------------------------
-  CREATE TABLE `t_stage` (
-    `id` varchar(64) NOT NULL,
-    `pipeline_version_id` varchar(64) NULL DEFAULT NULL COMMENT '流水线id',
-    `build_id` varchar(64) NULL DEFAULT NULL,
-    `status` varchar(100) NULL DEFAULT NULL COMMENT '构建状态',
-    `exit_code` bigint(20) NULL DEFAULT NULL COMMENT '退出码',
-    `error` varchar(500) NULL DEFAULT NULL COMMENT '错误信息',
-    `name` varchar(255) NULL DEFAULT NULL COMMENT '名字',
-    `display_name` varchar(255) NULL DEFAULT NULL,
-    `started` datetime(0) NULL DEFAULT NULL COMMENT '开始时间',
-    `finished` datetime(0) NULL DEFAULT NULL COMMENT '结束时间',
-    `created` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-    `updated` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-    `version` varchar(255) NULL DEFAULT NULL COMMENT '版本',
-    `on_success` varchar(5) NULL DEFAULT NULL,
-    `on_failure` varchar(5) NULL DEFAULT NULL,
-    `sort` bigint(10) NULL DEFAULT NULL,
-    `stage` varchar(255) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
-CREATE TABLE `t_variable` (
-    `id` varchar(64) NOT NULL,
-    `name` varchar(255) NULL DEFAULT NULL,
-    `value` varchar(255) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
 CREATE TABLE `t_user` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `name` varchar(100) NULL DEFAULT NULL,
-    `pass` varchar(255) NULL DEFAULT NULL,
-    `nick` varchar(100) NULL DEFAULT NULL,
-    `avatar` varchar(500) NULL DEFAULT NULL,
-    `created` datetime(0) NULL DEFAULT NULL,
-    `login_time` datetime(0) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
+  `id` varchar(64) NOT NULL,
+  `aid` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NULL DEFAULT NULL,
+  `pass` varchar(255) NULL DEFAULT NULL,
+  `nick` varchar(100) NULL DEFAULT NULL,
+  `avatar` varchar(500) NULL DEFAULT NULL,
+  `created` datetime(0) NULL DEFAULT NULL,
+  `login_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `aid`) USING BTREE
+);
 -- ----------------------------
-  -- Records of t_user
-  -- ----------------------------
+-- Records of t_user
+-- ----------------------------
 INSERT INTO
   `t_user`
 VALUES
@@ -375,34 +181,33 @@ VALUES
     NOW(),
     NULL
   );
--- ----------------------------
-  -- Table structure for t_user_msg
-  -- ----------------------------
-  CREATE TABLE `t_user_msg` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `mid` varchar(64) NULL DEFAULT NULL,
+CREATE TABLE `t_user_org` (
+    `aid` bigint(20) NOT NULL AUTO_INCREMENT,
+    `uid` varchar(64) NULL DEFAULT NULL,
+    `org_id` varchar(64) NULL DEFAULT NULL,
+    `created` datetime(0) NULL DEFAULT NULL,
+    `perm_adm` INT(1) NULL DEFAULT 0 COMMENT '管理员',
+    `perm_rw` INT(1) NULL DEFAULT 0 COMMENT '1只读,2读写',
+    `perm_exec` INT(1) NULL DEFAULT 0 COMMENT '执行权限',
+    PRIMARY KEY (`aid`) USING BTREE,
+    INDEX `uid`(`uid`) USING BTREE,
+    INDEX `oid`(`org_id`) USING BTREE,
+    INDEX `uoid`(`uid`, `org_id`) USING BTREE
+  );
+CREATE TABLE `t_user_msg` (
+    `aid` BIGINT NOT NULL AUTO_INCREMENT,
     `uid` varchar(64) NULL DEFAULT NULL COMMENT '收件人',
+    `msg_id` varchar(64) NULL DEFAULT NULL,
     `created` datetime(0) NULL DEFAULT NULL,
     `readtm` datetime(0) NULL DEFAULT NULL,
     `status` int(11) NULL DEFAULT 0,
     `deleted` int(1) NULL DEFAULT 0,
     `deleted_time` datetime(0) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
-CREATE TABLE `t_user_repo` (
-    `id` varchar(64) NOT NULL,
-    `repo_id` varchar(64) NOT NULL,
-    `user_id` bigint(20) NOT NULL,
-    PRIMARY KEY (`id`) USING BTREE
-  );
-CREATE TABLE `t_user_role` (
-    `user_id` bigint(20) NOT NULL,
-    `role_codes` text NULL,
-    `limits` text NULL,
-    PRIMARY KEY (`user_id`) USING BTREE
+    PRIMARY KEY (`aid`) USING BTREE,
+    INDEX `uid`(`uid`) USING BTREE
   );
 CREATE TABLE `t_user_token` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `aid` bigint(20) NOT NULL AUTO_INCREMENT,
     `uid` bigint(20) NULL DEFAULT NULL,
     `type` varchar(50) NULL DEFAULT NULL,
     `openid` varchar(100) NULL DEFAULT NULL,
@@ -417,5 +222,7 @@ CREATE TABLE `t_user_token` (
     `created` datetime(0) NULL DEFAULT NULL,
     `tokens` text NULL,
     `uinfos` text NULL,
-    PRIMARY KEY (`id`) USING BTREE
+    PRIMARY KEY (`aid`) USING BTREE,
+    INDEX `uid`(`uid`) USING BTREE,
+    INDEX `openid`(`openid`) USING BTREE
   );
