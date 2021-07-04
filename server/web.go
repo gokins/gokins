15c0f65fb233b71776227a560854e979d006ffda
LinskRuis.32G <mgr9525@qq.com>
2021-07-04 21:25:30 +0800

    bin ui+-

--- server/web.go
+++ server/web.go
@@ -1,6 +1,13 @@
 package server
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/base64"
+	"errors"
+	"github.com/gokins-main/core/utils/httpex"
+	"net/http"
+	"path/filepath"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +26,7 @@ func runWeb() {
 		}
 	}()
 	comm.WebEgn = gin.Default()
+	comm.WebEgn.Use(midUiHandle)
 	err := comm.WebEgn.Run(comm.WebHost)
 	if err != nil {
 		logrus.Errorf("Web err:%v", err)
@@ -40,6 +48,92 @@ func regApi() {
 	util.GinRegController(comm.WebEgn, &route.RuntimeController{})
 	util.GinRegController(comm.WebEgn, &route.YmlController{})
 }
-func http404(c *gin.Context) {
+func midUiHandle(c *gin.Context) {
+	c.Next()
+	if c.Writer.Status() != http.StatusNotFound || c.Writer.Size() > 0 {
+		return
+	}
+	pth := c.Request.RequestURI
+	if !comm.Installed && pth != "/install" {
+		httpex.ResMsgUrl(c, "未安装,跳转中...", "/install")
+		return
+	}
+	r, err := getFile(pth)
+	if err != nil {
+		r, err = getFile("/index.html")
+	}
+	if err != nil {
+		//c.String(404, "rdr err:"+err.Error())
+		httpex.ResMsgUrl(c, "未找到内容,跳转中...", "/")
+		return
+	}
+	rd, err := r.Open()
+	if err != nil {
+		//c.String(500, "open err:"+err.Error())
+		httpex.ResMsgUrl(c, "内容有误,跳转中...", "/")
+		return
+	}
+	defer rd.Close()
+	c.Writer.Header().Set("Cache-Control", "max-age=360000000")
 
+	ext := filepath.Ext(r.Name)
+	if ext == ".html" {
+		c.Writer.Header().Set("Cache-Control", "no-cache")
+		c.Writer.Header().Set("Pragma", "no-cache")
+		c.Writer.Header().Set("Expires", "0")
+		c.Writer.Header().Set("Content-Type", "text/html")
+	} else if ext == ".css" {
+		c.Writer.Header().Set("Content-Type", "text/css")
+	} else if ext == ".js" {
+		c.Writer.Header().Set("Content-Type", "application/javascript")
+	} else if ext == ".svg" {
+		c.Writer.Header().Set("Content-Type", "image/svg+xml")
+	}
+	c.Status(200)
+	bts := make([]byte, 1024)
+	for !hbtp.EndContext(c) {
+		n, err := rd.Read(bts)
+		if n > 0 {
+			c.Writer.Write(bts[:n])
+		}
+		if err != nil {
+			break
+		}
+	}
+}
+
+var rder *zip.Reader
+
+func getRdr() (*zip.Reader, error) {
+	if rder != nil {
+		return rder, nil
+	}
+	bts, err := base64.StdEncoding.DecodeString(comm.StaticPkg)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewReader(bts)
+	r, err := zip.NewReader(buf, buf.Size())
+	if err != nil {
+		return nil, err
+	}
+	rder = r
+	return rder, nil
+}
+func getFile(pth string) (*zip.File, error) {
+	if pth == "" {
+		return nil, errors.New("param err")
+	}
+	//println("getFile:" + pth)
+	r, err := getRdr()
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range r.File {
+		//println("f.name:", f.Name)
+		if pth == f.Name {
+			return f, nil
+		}
+	}
+	return nil, errors.New("file not found")
 }
