e7e183ac3cf6790caccde9c5b7454db2a4e2e268
jim <303600370@qq.com>
2021-07-07 10:54:36 +0800

    fix bugs

--- model/t_pipeline.go
+++ model/t_pipeline.go
@@ -10,7 +10,6 @@ type TPipeline struct {
 	Name         string    `xorm:"VARCHAR(255)" json:"name"`
 	DisplayName  string    `xorm:"VARCHAR(255)" json:"displayName"`
 	PipelineType string    `xorm:"VARCHAR(255)" json:"pipelineType"`
-	JsonContent  string    `xorm:"LONGTEXT" json:"jsonContent"`
 	YmlContent   string    `xorm:"LONGTEXT" json:"ymlContent"`
 	AccessToken  string    `xorm:"VARCHAR(255)" json:"accessToken"`
 	Url          string    `xorm:"VARCHAR(255)" json:"url"`
