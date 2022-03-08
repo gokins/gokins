6a42a1d607f9e5a7fc27544f2c9186780bfb74d7
LinsRuis.wubt20 <mgr9525@qq.com>
2022-03-08 15:36:41 +0800

    bug

--- engine/buildTaskc.go
+++ engine/buildTaskc.go
@@ -154,7 +154,7 @@ func (c *BuildTask) genRunjob(stage *runtime.Stage, job *jobSync) (rterr error) 
 		UseArtifacts: job.step.UseArtifacts,
 		OriginRepo:   c.repoPath,
 	}
-	if c.isClone || c.step.MustCopy {
+	if c.isClone || job.step.MustCopy {
 		runjb.OriginRepo = ""
 	}
 	var err error
