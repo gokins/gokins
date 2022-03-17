e193fb3c8b3a28abaec214673b278280cadf333a
LinsRuis.wubt20 <mgr9525@qq.com>
2022-03-17 15:27:28 +0800

    d

--- build_dcklc.sh
+++ build_dcklc.sh
@@ -1,5 +1,6 @@
 
 mkdir bin
+rm bin/gokins-alpine
 wget -O bin/gokins-alpine http://down.gokins.cn/static/golang/linux64/gokins-alpine
 chmod +x bin/gokins-alpine
 docker build -t mgr9525/gokins:latest .
